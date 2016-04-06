3b49a7fff7af39a682c38b9d7c9ece66c6b15227
borovelli <borovinwin@gmail.com>
2016-04-06 13:28:51 +0200

    with buffer

--- borovelli-javavelli/src/com/github/borovelli/javavelli/reverse/string/ReverseString.java
+++ borovelli-javavelli/src/com/github/borovelli/javavelli/reverse/string/ReverseString.java
@@ -1,12 +1,46 @@
 package com.github.borovelli.javavelli.reverse.string;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.*;
+
+import java.util.Scanner;
+
+import org.junit.Test;
+
 public class ReverseString {
 
-	/**
-	 * @param args
-	 */
+	@Test
+	public void testReverse() {
+		String input = "abc";
+		boolean testPassed = Reverse(input).equals("cba");
+		assertTrue(testPassed);
+		if (testPassed) {
+			System.out.println("Test passed");
+		}
+	}
+
+	public static String Reverse(String input) {
+		char[] borovin = input.toCharArray();
+		int L = borovin.length;
+		char[] buffer = new char[L + 1];
+		for (int i = 0; i < L; i++) {
+			buffer[L - i] = borovin[i];
+		}
+		String result = new String(buffer);
+		return result;
+
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+		String result = Reverse(input);
+
+		System.out.println("Original string " + input);
+		System.out.println("Reverse string " + result);
 
 	}
 
