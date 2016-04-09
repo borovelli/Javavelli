dfc5b317937e1f58f676e849770e7f111effcccd
borovelli <borovinwin@gmail.com>
2016-04-09 14:44:59 +0200

    base b to power of p with reccursion

---
+++ borovelli-javavelli/src/com/github/borovelli/javavelli/newton/PowReccursion.java
@@ -0,0 +1,42 @@
+package com.github.borovelli.javavelli.newton;
+
+//import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class PowReccursion {
+
+	public static double PowerReccursion(double b, double p) {
+		
+		if (inepsilondistance(p, 0)) {
+			return 1; // check if p is zero
+		}
+		double result = b * PowerReccursion(b, p - 1);
+		return result;
+	}
+
+	private static boolean inepsilondistance(double a, double b) {
+		double EPSILON = 1e-15;
+		if (Math.abs(a - b) < EPSILON) {
+			return true;
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("The base is");
+		@SuppressWarnings("resource")
+		Scanner base = new Scanner(System.in);
+		double b = base.nextDouble();
+		System.out.println("The power is");
+		Scanner power = new Scanner(System.in);
+		double p = power.nextDouble();
+
+		long beginTime = System.currentTimeMillis();
+		// timer starts in nanoseconds
+		System.out.println(PowerReccursion(b, p));
+		long endTime = System.currentTimeMillis(); // timer finishes
+		long difference = endTime - beginTime;
+		System.out.println(difference + " milliseconds ");
+	}
+}
