3c4f8465fa635cfc16aee0a54997f1ad38839697
borovelli <borovinwin@gmail.com>
2016-04-11 15:35:45 +0200

    now taking also negative X, and does not loop on small epsilon

--- borovelli-javavelli/src/com/github/borovelli/javavelli/newton/NewtonSqrt.java
+++ borovelli-javavelli/src/com/github/borovelli/javavelli/newton/NewtonSqrt.java
@@ -1,4 +1,3 @@
-
 package com.github.borovelli.javavelli.newton;
 
 import static org.junit.Assert.assertTrue;
@@ -11,33 +10,56 @@ import org.junit.Test;
 public class NewtonSqrt {
 
 	@Test
-	public void testReverse () {
-
+	public void testReverse() {
 		assertTrue(testRoot(2));
 		assertTrue(testRoot(3));
 		assertTrue(testRoot(64));
 		assertTrue(testRoot(121));
-
 	}
 
-	private boolean testRoot (double x) {
+	private boolean testRoot(double x) {
 		double rootNewton = sqrt(x);
 		double rootJava = StrictMath.sqrt(x);
 		return PowReccursion.inepsilondistance(rootNewton, rootJava, epsilon);
 	}
 
-	public static void main (String[] args) {
+	public static double sqrt(double x) {
+		if (x < 0) {
+			x = -x;
+			double y = x / 2; // sqrt(x) estimate
+			while (Math.abs(x / y - y) > epsilon * y) {
+				if (Math.abs(y - ((x / y + y) / 2.0)) <= epsilon) {
+					break;
+				}
+				System.out.println("Newton x  = i " + y);
+				y = (x / y + y) / 2.0;
+			}
+			return y;
+		} else {
+			double y = x / 2; // sqrt(x) estimate
+			while (Math.abs(x / y - y) > epsilon * y) {
+				if (Math.abs(y - ((x / y + y) / 2.0)) <= epsilon) {
+					break;
+				}
+				System.out.println("Newton x  = " + y);
+				y = (x / y + y) / 2.0;
+			}
+			return y;
+		}
+	}
 
+	public static void main(String[] args) {
+		System.out.println(" Enter the x value ");
 		@SuppressWarnings("resource")
 		Scanner input = new Scanner(System.in);
-		double c = input.nextDouble();
+		double x = input.nextDouble();
 		long beginTime = System.nanoTime();
 		// timer starts in nanoseconds
-		double t = sqrt(c);
-		double check = t * t;
-		System.out.println("   Newton x  = " + t);
-		System.out.println("   Math.sqrt " + Math.sqrt(c));
-		System.out.println("   x * x = " + check);
+		double y = sqrt(x);
+		double check = y * y;
+		System.out.println("Newton |x|  = " + y);
+		System.out.println("Math.sqrt " + Math.sqrt(x));
+		System.out.println(" Check  x * x = " + check);
 
 		// print out the estimate of the square root of c
 		long endTime = System.nanoTime(); // timer finishes
@@ -45,19 +67,6 @@ public class NewtonSqrt {
 		System.out.println(difference + " nanoseconds ");
 	}
 
-	static public double epsilon = 1e-15; // error tolerance
-
-	public static double sqrt (double c) {
-
-		double t = c / 2; // sqrt(c) estimate
-
-		while (Math.abs(c / t - t) > epsilon * t) {
-			// unless t is epsilon close to c
-			// take the t = (t and c/t) average
-//			System.out.println(t);
-			t = (c / t + t) / 2.0;
-		}
-		return t;
-	}
+	static public double epsilon = 1e-29; // error tolerance
 
 }
