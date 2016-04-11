178ebd4f261748b74a347d91f546498da01cf479
borovelli <borovinwin@gmail.com>
2016-04-11 14:19:02 +0200

    ok

--- borovelli-javavelli/src/com/github/borovelli/javavelli/newton/ExpBySquaring.java
+++ borovelli-javavelli/src/com/github/borovelli/javavelli/newton/ExpBySquaring.java
@@ -6,38 +6,38 @@ import java.util.Scanner;
 public class ExpBySquaring {
 
 	public static double expBySquaring(double b, double p) {
-		if (p < 0)
+		if (p < 0) {
 			return expBySquaring(1 / b, -p);
-		else if (p == 0)
+		}
+		if (p == 0) {
 			return 1;
-		else if (p == 1)
+		}
+		if (p == 1) {
 			return b;
-		else if (p % 2 == 0)
+		}
+		if (p % 2 == 0) {
+			System.out.println(b);
 			return expBySquaring(b * b, p / 2);
-		else
+		} else {
+			System.out.println(b);
 			return b * expBySquaring(b * b, (p - 1) / 2);
+		}
 	}
 
-	public static boolean inepsilondistance(double a, double b, double EPSILON) {
-		double distance = Math.abs(a - b);
-		return distance < EPSILON;
-	}
- 
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 
 		System.out.println("The base is");
 		Scanner base = new Scanner(System.in);
 		double b = base.nextDouble();
-		
 		System.out.println("The power is");
 		Scanner power = new Scanner(System.in);
 		double p = power.nextDouble();
 
 		long beginTime = System.currentTimeMillis();
-		
+
 		System.out.println(expBySquaring(b, p));
-		
+
 		long endTime = System.currentTimeMillis(); // timer finishes
 		long difference = endTime - beginTime;
 		System.out.println(difference + " milliseconds ");
