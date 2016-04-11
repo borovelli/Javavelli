1e2616dd98dda56801655268cb1dcb324b06a5fa
J Fixby <github@jfixby.com>
2016-04-11 17:18:58 +0200

    use long

--- borovelli-javavelli/src/com/github/borovelli/javavelli/sorting/IntMergeSort.java
+++ borovelli-javavelli/src/com/github/borovelli/javavelli/sorting/IntMergeSort.java
@@ -87,7 +87,7 @@ public class IntMergeSort {
 
 	public static void mergeSort (byte[] list, byte[] buffer, int lowIndex, int highIndex) {
 		if (lowIndex < highIndex) {
-			int midIndex = highIndex / 2 + lowIndex / 2;
+			int midIndex = (int)(((long)highIndex + (long)lowIndex) / 2L);
 			mergeSort(list, buffer, lowIndex, midIndex);
 			mergeSort(list, buffer, midIndex + 1, highIndex);
 			merge(list, buffer, lowIndex, midIndex, highIndex);
